2150a085e520809edbdd141c0c63ed0d73b1e9b1
Vlad Shevtsov <vladsh123@gmail.com>
2024-11-05 17:05:53 +0200

    ADD delete and post endpoints

--- app.py
+++ app.py
@@ -12,6 +12,13 @@ app = Flask(__name__)
 def get_stores():
     return {"stores": list(stores.values())}
 
+@app.get('/store/<string:store_id>')
+def get_store(store_id):
+    try:
+        return stores[store_id]
+    except KeyError:
+        abort(404, message="Store not found.")
+
 @app.post('/store')
 def create_store():
     store_data = request.get_json()
@@ -25,6 +32,24 @@ def create_store():
     stores[store_id] = store
     return store, 201
 
+@app.delete('/store/<string:store_id>')
+def delete_store(store_id):
+    try:
+        del stores[store_id]
+        return {"message": "Store deleted."}
+    except KeyError:
+        abort(404, message="Store not found.")
+
+@app.get('/item')
+def get_items():
+    return {"items": list(items.values())}
+
+@app.get('/item/<string:item_id>')
+def get_item(item_id):
+    try:
+        return items[item_id]
+    except KeyError:
+        abort(404, message="Item not found.")
 
 @app.post('/item')
 def create_item():
@@ -49,21 +74,22 @@ def create_item():
     items[item_id] = item
     return item, 201
 
-@app.get('/item')
-def get_items():
-    return {"items": list(items.values())}
-
-
-@app.get('/store/<string:store_id>')
-def get_store(store_id):
+@app.delete('/item/<string:item_id>')
+def delete_item(item_id):
     try:
-        return stores[store_id]
-    except IndexError:
-        abort(404, message="Store not found.")
-
-@app.get('/item/<string:item_id>')
-def get_item(item_id):
-    try:
-        return items[item_id]
-    except IndexError:
+        del items[item_id]
+        return {"message": "Item deleted."}
+    except KeyError:
         abort(404, message="Item not found.")
+
+@app.put('/item/<string:item_id>')
+def update_item(item_id):
+    item_data = request.get_json()
+    if "price" not in item_data or "name" not in item_data:
+        abort(400, message="Missing required fields (name or price).")
+    try:
+        item = items[item_id]
+        item |= item_data # merge dictionaries to update item
+    except KeyError:
+        abort(404, message="Item not found.")
+    return item
