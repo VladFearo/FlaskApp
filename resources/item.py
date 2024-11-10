0d10153ec2f86fe8997dff167d6c431b3b169313
Vlad Shevtsov <vladsh123@gmail.com>
2024-11-10 19:57:33 +0200

    ADD implement database queries for item and store retrieval, and update item handling

--- resources/item.py
+++ resources/item.py
@@ -15,28 +15,28 @@ blp = Blueprint("items", __name__, description="Item operations")
 class Item(MethodView):
     @blp.response(200, ItemSchema)
     def get(self, item_id):
-        try:
-            return items[item_id]
-        except KeyError:
-            abort(404, message="Item not found.")
+        item = ItemModel.query.get_or_404(item_id)
+        return item
 
     def delete(self, item_id):
-        try:
-            del items[item_id]
-            return {"message": "Item deleted."}
-        except KeyError:
-            abort(404, message="Item not found.")
+        item = ItemModel.query.get_or_404(item_id)
+        raise NotImplementedError("Delete operation not implemented yet.")
     
     @blp.arguments(ItemUpdateSchema)
     @blp.response(200, ItemSchema)
     def put(self,item_data, item_id):
-        try:
-            item = items[item_id]
-            item |= item_data # merge dictionaries to update item
-            
-            return item
-        except KeyError:
-            abort(404, message="Item not found.")
+        item = ItemModel.query.get(item_id)
+        
+        if item:
+            item.price = item_data["price"]
+            item.name = item_data["name"]
+        else:
+            item = ItemModel(id=item_id, **item_data)
+
+        db.session.add(item)    
+        db.session.commit()
+
+        return item
         
 
 @blp.route("/item")
