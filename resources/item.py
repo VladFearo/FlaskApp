4c9e6d1af9ac79c6bd0a0725a3e5eb80b2bf60aa
Vlad Shevtsov <vladsh123@gmail.com>
2024-11-10 18:15:33 +0200

    ADD integrate SQLAlchemy for item and store creation with error handling

--- resources/item.py
+++ resources/item.py
@@ -2,8 +2,11 @@ import uuid
 from flask import request
 from flask.views import MethodView
 from flask_smorest import abort, Blueprint
-from db import items
+from sqlalchemy.exc import SQLAlchemyError
+
+from db import db
 from schemas import ItemSchema, ItemUpdateSchema
+from models import ItemModel
 
 
 blp = Blueprint("items", __name__, description="Item operations")
@@ -45,14 +48,13 @@ class ItemList(MethodView):
     @blp.arguments(ItemSchema)
     @blp.response(201, ItemSchema)
     def post(self, item_data):
-        for item in items.values():
-            if(
-            item_data["name"] == item["name"]
-            and item_data["store_id"] == item["store_id"]
-            ):
-                abort(400, message="Item already exists.")
+        item = ItemModel(**item_data)
 
-        item_id = uuid.uuid4().hex
-        item = { **item_data, "id": item_id }
-        items[item_id] = item
-        return item, 201
+        try:
+            db.session.add(item)
+            db.session.commit()
+        except SQLAlchemyError:
+            abort(500, message="An error occurred while adding the item to the database.")
+        
+
+        return item
--- resources/store.py
+++ resources/store.py
@@ -2,8 +2,12 @@ import uuid
 from flask import request
 from flask.views import MethodView
 from flask_smorest import abort, Blueprint
-from db import stores
 from schemas import StoreSchema
+from sqlalchemy.exc import SQLAlchemyError, IntegrityError
+
+from models import StoreModel
+from db import db
+
 
 blp = Blueprint("stores", __name__, description="Store operations")
 
@@ -34,12 +38,14 @@ class StoreList(MethodView):
     @blp.arguments(StoreSchema)
     @blp.response(201, StoreSchema)
     def post(self, store_data):
-        for store in stores.values():
-            if store_data["name"] == store["name"]:
-                abort(400, message=f"Store already exists.")
-
-        store_id = uuid.uuid4().hex
-        store = {**store_data, "id": store_id}
-        stores[store_id] = store
+        store = StoreModel(**store_data)
+        try:
+            db.session.add(store)
+            db.session.commit()
+        except IntegrityError:
+            abort(400, message="A store with the same name already exists.")
+        except SQLAlchemyError:
+            abort(500, message="An error occurred while adding the store to the database.")
+        
 
         return store
