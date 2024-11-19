c8992b2f75a10a3a856be91e8fb7ad6da696ef1d
Vlad Shevtsov <vladsh123@gmail.com>
2024-11-19 18:49:28 +0200

    ADD implement database migration setup and add description field to item model

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask, jsonify
 from flask_smorest import Api
 from flask_jwt_extended import JWTManager
+from flask_migrate import Migrate
 
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
@@ -27,8 +28,9 @@ def create_app(db_url=None):
     app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL","sqlite:///data.db")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
-
+    migrate = Migrate(app, db)
     api = Api(app)
+    
     app.config["JWT_SECRET_KEY"] = "<Rpe9BDx{SuNva1" # Change this later
     jwt = JWTManager(app)
 
